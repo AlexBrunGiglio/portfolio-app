febae7eab6f077c5278384403786b240f39106f9
Alexandre <43376983+AlexBrunGiglio@users.noreply.github.com>
2020-06-30 11:42:04 +0200

    ajout menu + component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'about', component: AboutComponent},
+  { path : 'blog', component: BlogComponent},
+  { path : 'contact', component: ContactComponent },
+  { path : 'experience', component: ExperienceComponent },
+  { path : 'project', component: ProjectComponent},
+  { path : 'skills', component: SkillsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponent } from './project/project.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectComponent,
+    ExperienceComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
